bd0e2ccd850d930163f1fb68fd2e92750e0b8e44
Aarthy Bhaskar <s.b.aarthy@gmail.com>
2015-05-01 15:59:38 -0700

    Additional Converter methods

--- src/convert/Converter.java
+++ src/convert/Converter.java
@@ -4,44 +4,171 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HastaxonAuthorityMap;
 import java.util.List;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
 
 public class Converter {
 
-	public static void main(String[] args) throws JDOMException, IOException {
-		File input = new File("input");
-		File output = new File("output");
-		output.delete();
-		output.mkdir();
-		
-		for(File f : input.listFiles()) {
-			  SAXBuilder sax = new SAXBuilder();
-			  Document doc = sax.build(f);
-			  
-			  Element root = doc.getRootElement();
-			  List<Element> descriptions = root.getChildren("description");
-			  for(Element description : descriptions) {
-				  List<Element> statements =  description.getChildren("statement");
-				  for(Element statement : statements) {
-					  List<Element> structures = statement.getChildren("structure");
-					  for(Element structure : structures) {
-						  structure.setName("biological_entity");
-						  structure.setAttribute("type", "structure");
-					  }
-				  }
-			  }
-			  
-			  XMLOutputter xout = new XMLOutputter(Format.getPrettyFormat());
-			  xout.output(doc, new FileOutputStream(new File("output", f.getName())));
+	
+	/*
+	 * Used to set "unknown" to empty tags
+	 *  paths - takes list of tags that are non-empty and that need to updated
+	 */
+	public static Document correctNonEmptyTags(Document doc, ArrayList<String> paths){
+		XPathFactory xFactory = XPathFactory.instance();
+		for(String path: paths){
+			XPathExpression<Element> expression = xFactory.compile(path, Filters.element());
+			List<Element> elements = expression.evaluate(doc);
+			for(Element elem : elements){
+				if(elem.getText().length() == 0){
+					elem.setText("unknown");
+				}
+			}
 		}
 		
+		return doc;
+	}
+	
+	/*
+	 * Used to set authority for taxons
+	 * taxonAuthorityMap - pairs of (taxonnames,authority)
+	 * Can be modified to take taxonDateMap (date is set to unknown now)
+	 */
+	public static Document correctTaxon(Document jdomDocument, HastaxonAuthorityMap<String, String> taxonAuthorityMap){
+		XPathFactory xFactory = XPathFactory.instance();
+		XPathExpression<Element> expression = xFactory.compile("//taxon_identification/taxon_name", Filters.element());
+		List<Element> elements = expression.evaluate(jdomDocument);
+		for(Element elem : elements){
+				String authority_value;
+				if(taxonAuthorityMap.get(elem.getText())!=null){
+					authority_value = taxonAuthorityMap.get(elem.getText());
+				}else{
+					authority_value = "unknown";
+				}
+				elem.setAttribute("authority", authority_value);
+				elem.setAttribute("date", "unknown");
+		}
+		return jdomDocument;
+	}
+	
+	/*
+	 * Used to rename tags 
+	 * tagNamesMap - pairs of (complete_path_of_old_name, newname)
+	 */
+	public static Document renameTags(Document doc, HashMap<String, String> tagNamesMap){
+		XPathFactory xFactory = XPathFactory.instance();
+		for(String key: tagNamesMap.keySet()){
+			XPathExpression<Element> expression = xFactory.compile(key, Filters.element());
+			List<Element> elements = expression.evaluate(doc);
+			for(Element elem : elements){
+				elem.setName(tagNamesMap.get(key));
+			}
+		}
+		return doc;
+	}
+	
+	/*
+	 * Used to rename attributes
+	 * attributeNamesMap - pairs of  attribute's (complete_path_of_old_name, newname)
+	 */
+	
+	public static Document renameAttributes(Document doc, HashMap<String, String> attributeNamesMap){
+		XPathFactory xFactory = XPathFactory.instance();
+		for(String key: attributeNamesMap.keySet()){
+			XPathExpression<Attribute> expression = xFactory.compile(key, Filters.attribute());
+			List<Attribute> elements = expression.evaluate(doc);
+			for(Attribute elem : elements){
+				elem.setName(attributeNamesMap.get(key));
+			}
+		}
+		return doc;
+	}
+	
+	/*
+	 * Used to move an element to a destination
+	 * arrangementMap - pairs of (complete_path_of_element_to_be_moved, complete_path_of_destination_parent)
+	 */
+	
+	public static Document rearrangeTags(Document jDomDocument,
+			HashMap<String, String> arrangementMap) {
+		XPathFactory xFactory = XPathFactory.instance();
+		for(String key: arrangementMap.keySet()){
+			XPathExpression<Element> expression1 = xFactory.compile(key, Filters.element());
+			XPathExpression<Element> expression2 = xFactory.compile(arrangementMap.get(key), Filters.element());
+			List<Element> source = expression1.evaluate(jDomDocument);
+			List<Element> destination = expression2.evaluate(jDomDocument);
+			int count = 0;
+			for(Element elem : source){
+				Element parent  = elem.getParentElement();
+				parent.removeChild(elem.getName());
+				Element dest = destination.get(count); 
+				dest.addContent(dest.getContentSize(), elem);
+				count++;
+			}
+		}
+		return jDomDocument;
+	}
+
+	/*
+	 * Adds meta tag to document that does not have one
+	 */
+	
+	public static Document addMetaTag(Document jdomDocument) {
+		XPathFactory xFactory = XPathFactory.instance();
+		XPathExpression<Element> expression = xFactory.compile("//meta", Filters.element());
+		List<Element> elements = expression.evaluate(jdomDocument);
+		if(elements.isEmpty()){
+			Element metaTag = new Element("meta");
+			Element sourceTag = new Element("source");
+			sourceTag.addContent(new Element("author").setText("unknown"));
+			sourceTag.addContent(new Element("date").setText("unknown"));
+			metaTag.addContent(sourceTag);
+			jdomDocument.getRootElement().addContent(0, metaTag);
+		}
+		return jdomDocument;
+	}
+	
+	/*
+	 * Adds type attribute to description elements
+	 */
+	public static Document addTypeAttribute(Document jdomDocument) {
+		XPathFactory xFactory = XPathFactory.instance();
+		XPathExpression<Element> expression = xFactory.compile("//description", Filters.element());
+		List<Element> elements = expression.evaluate(jdomDocument);
+		for(Element elem : elements){
+			if(elem.getAttribute("type") == null){
+				elem.setAttribute("type", "morphology");
+			}
+		}
+		return jdomDocument;
+	}
+	
+	/*
+	 * sets the namespace to the xml file
+	 */
+
+	public static Document setNameSpace(Document jdomDocument){
+		Namespace bioNamespace = Namespace.getNamespace("bio", "http://www.github.com/biosemantics");
+		Namespace xsiNamespace = Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+		Element rootElement = jdomDocument.getRootElement();
+		rootElement.setNamespace(bioNamespace);
+		rootElement.addNamespaceDeclaration(bioNamespace);
+		rootElement.addNamespaceDeclaration(xsiNamespace);
+		rootElement.setAttribute("schemaLocation", "http://www.github.com/biosemantics http://www.w3.org/2001/XMLSchema-instance", xsiNamespace);
+		return jdomDocument;
 	}
 	
 }
---
+++ src/convert/MainClass.java
@@ -0,0 +1,142 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
+import convert.Converter;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		convert();
+		//Validator();
+	}
+	
+	
+	static void convert(){
+		//Give input folder here
+		File input = new File("ETC-FNA-v2_output3_familycopied_authoritycopied");
+		//Output folder name here
+		File output = new File("ETC-FNA-v2_output_final");
+		output.delete();
+		output.mkdir();
+		
+		SAXBuilder jdomBuilder = new SAXBuilder();
+        Document jdomDocument;
+		try {
+			for(File f : input.listFiles()) {
+				try {
+					jdomDocument = jdomBuilder.build(f);
+					//Different methods in Converter can be called
+					// The parameters to the methods need to be constructed here
+					// Can also be done as:
+					// renameTags(jdomDocument); - check the renameTags function below
+					// renameAttributes(jdomDocument); - check the renameAttributes function below 
+					jdomDocument = Converter.addMetaTag(jdomDocument);
+					jdomDocument = Converter.correctTaxonNames(jdomDocument, authorityList);
+					jdomDocument = Converter.correctDescriptions(jdomDocument);
+					XMLOutputter xout = new XMLOutputter(Format.getPrettyFormat());
+					xout.output(jdomDocument, new FileOutputStream(new File(output, f.getName())));
+				} catch (JDOMException e) {
+					System.out.println(f.getName()+"has jdom exception: "+e.getMessage());
+					continue;
+				}
+			}
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static Document renameTags(Document jdomDocument){
+		HashMap<String, String> namesMap = new HashMap<String, String>();
+		
+		namesMap.put("//TaxonIdentification", "taxon_identification");
+		namesMap.put("//TaxonHierarchy", "taxon_hierarchy");
+		
+		return Converter.renameTags(jdomDocument, namesMap);
+	}
+	
+	static Document renameAttributes(Document jdomDocument){
+		HashMap<String, String> namesMap = new HashMap<String, String>();
+		
+		namesMap.put("//taxon_identification/@Status", "status");
+		
+		return Converter.renameAttributes(jdomDocument, namesMap);
+	}
+	
+	static Document reArrangeTags(Document jDomDocument){
+		HashMap<String, String> arrangementMap = new HashMap<String, String>();
+		
+		arrangementMap.put("//taxon_hierarchy", "//taxon_identification");
+		
+		return Converter.rearrangeTags(jDomDocument, arrangementMap);
+	}
+
+	static Document convert_nonEmptyTag(Document jdomDocument){
+		ArrayList<String> paths = new ArrayList<String>();
+		paths.add("//number");
+		paths.add("//meta/source/author");
+		paths.add("//meta/source/date");
+		paths.add("//meta/source/title");
+		paths.add("//meta/source/pages");
+		paths.add("//meta/other_info_on_meta/@type");
+		paths.add("//processed_by/processor/date");
+		paths.add("//processed_by/processor/operator");
+		paths.add("//processed_by/processor/software/@*");
+		paths.add("//processed_by/processor/resource/@*");
+		paths.add("//taxon_identification/taxon_heirarchy");
+		paths.add("//taxon_identification/place_of_publication/publication_title");
+		paths.add("//taxon_identification/place_of_publication/place_in_publication");
+		paths.add("//taxon_identification/place_of_publication/other_info_on_pub/@*");
+		paths.add("//taxon_identification/other_info_on_name/@*");
+		paths.add("//taxon_identification/taxon_name/@*");
+		paths.add("//taxon_identification/strain_number/@*");
+		paths.add("//description/@type");
+		paths.add("//type/@*");
+		paths.add("//other_name/@*");
+		paths.add("//material/@*");
+		paths.add("//discussion/@*");
+		paths.add("//taxon_relation_articulation/@*");
+		paths.add("//key/key_heading");
+		paths.add("//key/key_author");
+		paths.add("//key/discussion");
+		paths.add("//key/key_head");
+		paths.add("//key/key_statement/statement_id");
+		paths.add("//key/key_statement/next_statement_id");
+		paths.add("//key/key_statement/description/@*");
+		
+		return Converter.correctNonEmptyTags(jdomDocument, paths);
+		
+	}
+	
+	static String readFile(String path, Charset encoding) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
+
+}
